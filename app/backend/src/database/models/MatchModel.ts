672f49e1abcbfedcfccc5761add48eaa6ed392fc
yasmintokunaga <yasmintkng@gmail.com>
2023-09-14 13:40:18 -0300

    feat(REQ15): create the endpoint GET /matches

--- app/backend/src/database/models/MatchModel.ts
+++ app/backend/src/database/models/MatchModel.ts
@@ -1,14 +1,19 @@
 import { IMatchModel } from '../../Interfaces/IMatchModel';
 import SequelizeMatch from './SequelizeMatch';
 import IMatch from '../../Interfaces/IMatch';
+import SequelizeTeam from './SequelizeTeam';
 
 export default class MatchModel implements IMatchModel {
   private model = SequelizeMatch;
 
   async findAll(): Promise<IMatch[]> {
-    const dbData = await this.model.findAll();
-    return dbData.map((
-      { id, homeTeamId, homeTeamGoals, awayTeamId, awayTeamGoals, inProgress },
-    ) => ({ id, homeTeamId, homeTeamGoals, awayTeamId, awayTeamGoals, inProgress }));
+    const dbData = await this.model.findAll({
+      include: [
+        { model: SequelizeTeam, as: 'homeTeam', attributes: ['teamName'] },
+        { model: SequelizeTeam, as: 'awayTeam', attributes: ['teamName'] },
+      ],
+    });
+
+    return dbData;
   }
 }
